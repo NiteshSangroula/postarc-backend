be2148905c6d719ac41411231fc6535f2cecb4f9
NiteshSangroula <sangroulanitesh123@gmail.com>
2025-11-15 08:31:56 +0545

    Initial commit

---
+++ src/test/java/com/postarc/postarc_backend/PostArcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.postarc.postarc_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostArcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

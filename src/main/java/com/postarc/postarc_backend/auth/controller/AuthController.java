06eb828f1bf1d9e804a5a37cf339dca7faa92943
NiteshSangroula <sangroulanitesh123@gmail.com>
2025-11-17 19:32:42 +0545

    feat(auth): add register endpoint in AuthController

---
+++ src/main/java/com/postarc/postarc_backend/auth/controller/AuthController.java
@@ -0,0 +1,29 @@
+package com.postarc.postarc_backend.auth.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.postarc.postarc_backend.auth.dto.RegisterRequest;
+import com.postarc.postarc_backend.auth.service.AuthService;
+import com.postarc.postarc_backend.users.dto.UserResponse;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<UserResponse> register(@Valid @RequestBody RegisterRequest request) {
+        UserResponse user = authService.register(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+
+    }
+}
